b4486df1b8976e1d77a7ad3e5c10a24e4dadbdd9
ashmit1795 <ashmitpatra77@gmail.com>
2026-04-19 19:09:58 +0530

    Add program to check if three positive integers can form a triangle

---
+++ GFG Java/L02/program6.java
@@ -0,0 +1,30 @@
+/*
+Ques: Take 3 positive integers input and
+tell if they can be the sides of a triangle
+or not.
+
+-> Sum of any two sides of the triangle should be greater than the 3rd one.
+*/
+
+import java.util.Scanner;
+
+public class program6 {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st side: ");
+        a = sc.nextInt();
+        System.out.print("Enter 2nd side: ");
+        b = sc.nextInt();
+        System.out.print("Enter 3rd side: ");
+        c = sc.nextInt();
+
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("Triangle is valid.");
+        } else {
+            System.out.println("Triangle is not valid.");
+        }
+
+        sc.close();
+    }
+}
