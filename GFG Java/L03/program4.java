70aafec45dcfd3aebe7694d81a92611dc474176c
ashmit1795 <ashmitpatra77@gmail.com>
2026-04-23 12:43:06 +0530

    Add program to print all alphabets with their corresponding ASCII values

---
+++ GFG Java/L03/program4.java
@@ -0,0 +1,12 @@
+/* 
+Ques: Print all alphabets with their
+corresponding ASCII values. 
+*/
+
+class program4 {
+    public static void main(String[] args) {
+        for (int i = 65; i <= 90; i++){
+            System.out.println((char)i + "-" +i);
+        }
+    }
+}
