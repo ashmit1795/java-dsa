92017143f36a9da8dfa185e5cea84408dcf8b55e
ashmit1795 <ashmitpatra77@gmail.com>
2026-05-06 23:29:03 +0530

    Add program to implement binary search on a reverse sorted array

---
+++ GFG Java/L09 - Binary Search/program4.java
@@ -0,0 +1,50 @@
+/*
+Binary Search on reverse sorted array
+*/
+
+import java.util.Scanner;
+
+class program4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter no. of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // input array elements
+        System.out.print("Enter array elements: ");
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.print("Enter target element: ");
+        int target = sc.nextInt();
+
+        int found = binarySearch(arr, target);
+        if (found == -1) {
+            System.out.println("Target element not found.");
+        } else {
+            System.out.println("Taget element found at index: " + found);
+        }
+
+        sc.close();
+    }
+
+    static int binarySearch(int arr[], int target) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                high = mid - 1; // go left
+            } else if (arr[mid] > target) {
+                low = mid + 1; // go right
+            }
+        }
+
+        return -1;
+    }
+}
