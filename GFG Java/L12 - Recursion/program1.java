6366b5c3cb7be719f3c345f8dd4cc163eb9c4a55
ashmit1795 <ashmitpatra77@gmail.com>
2026-05-20 11:55:08 +0530

    Add program to print numbers from n to 1 using recursion

---
+++ GFG Java/L12 - Recursion/program1.java
@@ -0,0 +1,15 @@
+/*
+Print n to 1 using recursion
+*/
+
+class program1{
+	public static void main(String[] args){
+		print(5);
+	}
+	
+	static void print(int n){
+		if(n == 0) return;
+		System.out.println(n);
+		print(n - 1);
+	}
+}
